7f49d7b153063a2ad7e192cc987a6d8284cef5b0
Gróf Richárd László <grof.richard.laszlo.21i@egressy.info>
2026-03-18 10:56:14 +0100

    ADDRESS PUT

--- Address.js
+++ Address.js
@@ -5,7 +5,19 @@ const dbhandler = require("./dbhandler.js");
 const auth = require("./Auth.js");
 
 
-
+router.post("/AddressRegister", auth(), async (req, res) => {
+    try {
+        await dbhandler.Address.create({
+            City: req.body.City,
+            UserId: req.uid, 
+            Zip: req.body.Zip,
+            Address1: req.body.Address1
+        });
+        return res.status(201).json({ message: "Sikeres Lakhely regisztráció" });
+    } catch (err) {
+        return res.status(500).json({ message: "Hiba a mentés során" });
+    }
+});
 
 
 
