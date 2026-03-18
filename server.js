98b53e07bfbd7a63f7c3bd77019e96635682083d
Gróf Richárd László <grof.richard.laszlo.21i@egressy.info>
2026-03-18 10:44:13 +0100

    server.js

--- server.js
+++ server.js
@@ -13,6 +13,41 @@ const Company = require("./Company");
 const server = express();
 const path = require('path');
 
+server.use(cors()); 
+server.use(express.json());
+server.use(express.static("public",{extensions: ["html"] }));
+server.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+server.use((req, res, next) => {
+    console.log(`>>> BEÉRKEZŐ KÉRÉS: ${req.method} ${req.url}`);
+    next();
+});
 
+const PORT = process.env.PORT || 3000;
+server.get(['/products', '/products/:brand'], (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'products.html'));
+});
+
+server.use("/", Company);
+server.use("/", User);
+server.use("/", Order);
+server.use("/", Products);
+server.use("/", Address);
+server.use("/", BillingAddress);
+server.use("/", Log);
+server.use((req, res) => {
+    res.status(404).redirect('/404');
+});
+db.dbhandler.sync()
+  .then(() => {
+    console.log("-----------------------------------------");
+    server.listen(PORT, () => {
+      console.log(`Szerver állapot: FUT (Port: ${PORT})`);
+      console.log("-----------------------------------------");
+    });
+  })
+  .catch((error) => {
+    console.error("KRITIKUS HIBA");
+    console.error(error);
+  });
 
 module.exports = server;
