08922140df71bbe17f5e13efc388071c642fb24e
Gróf Richárd László <grof.richard.laszlo.21i@egressy.info>
2026-03-18 10:49:34 +0100

    dbHandler USER

--- dbhandler.js
+++ dbhandler.js
@@ -5,7 +5,33 @@ const dbhandler = new Sequelize("sql_notrox_hu", "sql_notrox_hu", "6b107270b787b
     host: "127.0.0.1",
 });
 
-
+const UserTable = dbhandler.define("User", {
+    Id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    Username: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    Email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    Password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    Pfp: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    isAdmin: {
+        type: DataTypes.BOOLEAN,
+        allowNull: true
+    },
+});
 
 
 
