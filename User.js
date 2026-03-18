1f9651d39a55c422d0a12844a52eb6eebf7465bd
Gróf Richárd László <grof.richard.laszlo.21i@egressy.info>
2026-03-18 10:30:55 +0100

    USER multer

--- User.js
+++ User.js
@@ -13,7 +13,20 @@ const auth = require("./Auth.js");
 const sk = process.env.SECRET_KEY;
 const expiresin = process.env.EXPIRES_IN;
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        const dir = './uploads/';
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir);
+        }
+        cb(null, dir);
+    },
+    filename: function (req, file, cb) {
+        cb(null, `profile-${req.uid || 'anon'}-${Date.now()}${path.extname(file.originalname)}`);
+    }
+});
 
+const upload = multer({ storage: storage });
 
 
 
