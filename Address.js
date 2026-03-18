877c24f6c25a5631d6effbd2c5c10761b362d15b
Gróf Richárd László <grof.richard.laszlo.21i@egressy.info>
2026-03-18 10:56:39 +0100

    ADDRESS PUT

--- Address.js
+++ Address.js
@@ -36,6 +36,31 @@ router.get("/AddressGets", auth(), async (req, res) => {
     }
 });
 
+router.put("/EditAddress", auth(), async (req, res) => {
+    try {
+        const userId = req.uid; 
+        const address = await dbhandler.Address.findOne({ where: { UserId: userId } });
+        
+        if (!address) {
+            return res.status(404).json({ "message": "Nincs mentett cím ehhez a felhasználóhoz" });
+        }
+        
+        if (!req.body.City || !req.body.Zip || !req.body.Address1) {
+            return res.status(400).json({ "message": "Hiányzó adatok" });
+        }
 
+        await dbhandler.Address.update({
+            City: req.body.City,
+            Zip: req.body.Zip,
+            Address1: req.body.Address1
+        }, {
+            where: { UserId: userId }
+        });
+        
+        return res.status(200).json({ "message": "Sikeres módosítás" });
+    } catch (err) {
+        return res.status(500).json({ message: "Szerver hiba" });
+    }
+});
 
 module.exports = router;
