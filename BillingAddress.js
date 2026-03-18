5570dff8b8520d0f8663325288507fb0c165758f
Gróf Richárd László <grof.richard.laszlo.21i@egressy.info>
2026-03-18 10:54:33 +0100

    BILLINGADDRESS POST

--- BillingAddress.js
+++ BillingAddress.js
@@ -5,6 +5,19 @@ const dbhandler = require("./dbhandler.js");
 const auth = require("./Auth.js");
 
 
+router.post("/BillingAddressRegister", auth(), async (req, res) => {
+    try {
+        await dbhandler.BillingAddress.create({
+            City: req.body.City,
+            UserId: req.uid,
+            Zip: req.body.Zip,
+            Address1: req.body.Address1
+        });
+        return res.status(201).json({ message: "Sikeres számlázási cím regisztráció" });
+    } catch (err) {
+        return res.status(500).json({ message: "Hiba a mentés során" });
+    }
+});
 
 
 
