526bad0cb3b48161dbe7987b4c7cd79487d9d351
Gróf Richárd László <grof.richard.laszlo.21i@egressy.info>
2026-03-18 10:54:59 +0100

    BILLINGADDRESS PUT

--- BillingAddress.js
+++ BillingAddress.js
@@ -37,5 +37,31 @@ router.get("/BillingAddressGets", auth(), async (req, res) => {
 });
 
 
+router.put("/EditBillingAddress", auth(), async (req, res) => {
+    try {
+        const userId = req.uid;
+        const address = await dbhandler.BillingAddress.findOne({ where: { UserId: userId } });
+        
+        if (!address) {
+            return res.status(404).json({ "message": "Nincs mentett számlázási cím" });
+        }
+        
+        if (!req.body.City || !req.body.Zip || !req.body.Address1) {
+            return res.status(400).json({ "message": "Hiányzó adatok" });
+        }
+
+        await dbhandler.BillingAddress.update({
+            City: req.body.City,
+            Zip: req.body.Zip,
+            Address1: req.body.Address1
+        }, {
+            where: { UserId: userId }
+        });
+        
+        return res.status(200).json({ "message": "Sikeres módosítás" });
+    } catch (err) {
+        return res.status(500).json({ message: "Szerver hiba" });
+    }
+});
 
 module.exports = router;
