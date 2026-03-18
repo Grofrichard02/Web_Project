ba31d5ee036027f14b37b47c743cad8d5e505f3d
Gróf Richárd László <grof.richard.laszlo.21i@egressy.info>
2026-03-18 10:53:07 +0100

    COMPANY POST

--- Company.js
+++ Company.js
@@ -3,6 +3,26 @@ const router = express()
 const dbhandler=require("./dbhandler.js")
 const Auth = require("./Auth.js");
 
+router.post("/postCompany",Auth(),async(req,res)=>{
+    const onecompany=await dbhandler.CompanyTable.FindOne({
+        where:{
+            Name:req.body.Name,
+            Description:req.body.Description,
+            Location:req.body.Location
+        }
+    })
+    if(!onecompany){
+        await dbhandler.CompanyTable.create({
+            Name:req.body.Name,
+            Description:req.body.Description,
+            Location:req.body.Location
+        })
+        res.status(200).json({"message":"Sikeres létrehozás"})
+    }
+    else{
+        res.status(409).json({"message":"Sikertelen létrehozás"})
+    }
+})
 
 
 
